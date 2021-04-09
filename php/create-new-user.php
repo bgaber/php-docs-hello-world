281d28c282caa28f067eb978fc2d375e0755fe81
Brian Gaber <brian.gaber@primus.ca>
2021-04-09 11:39:19 -0400

    test read of environment variable

--- php/create-new-user.php
+++ php/create-new-user.php
@@ -59,8 +59,8 @@ if (isset($_POST['uid']) and $_POST['uid'] != "") {
 }
 
 function initial_html() {
-    $env = getenv("TESTENV");
-    echo "Test Environment Variable read from App Service Applicatin Settings: <b>$env</b><br>\n";
+    $environment_variable = getenv("TESTENV");
+    echo "Test Environment Variable read from App Service Application Settings: <b>$environment_variable</b><br>\n";
 	echo "<form method='post' enctype='multipart/form-data' name='add_user'>\n";
 	echo "	<center>\n";
 	echo "		<table width='95%'>\n";
