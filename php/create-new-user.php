3743faa23824037406803409c74326291b487bbf
Brian Gaber <brian.gaber@primus.ca>
2021-04-09 11:10:34 -0400

    env var test

--- php/create-new-user.php
+++ php/create-new-user.php
@@ -59,8 +59,8 @@ if (isset($_POST['uid']) and $_POST['uid'] != "") {
 }
 
 function initial_html() {
-    $env = getenv("TESTENV")
-    echo "<b>$env</b>"
+    $env = getenv("TESTENV");
+    echo "<b>$env</b>\n";
 	echo "<form method='post' enctype='multipart/form-data' name='add_user'>\n";
 	echo "	<center>\n";
 	echo "		<table width='95%'>\n";
